4473fd79965d233a8a0f629205b6ac33cec8ee96
vasukumgit <vasudevanleela25@gmail.com>
2026-03-16 16:02:51 +0530

    Update var.tf

--- Desktop/tr/terraform1/var.tf
+++ Desktop/tr/terraform1/var.tf
@@ -10,7 +10,7 @@ variable "ami_id"{
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
-  default     = "t2.micro"
+  default     = "t3.small"
 }
 variable "key_name" {
   description = "jsm.pem"
